4d8d84dbd3a2624382360ab34aa0db03112d7173
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-11 09:38:19 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """A script that obfuscates sensitive data."""
-from typing import List
+from typing import List, Sequence
 import re
+import logging
 
 
 def filter_datum(
@@ -21,3 +22,21 @@ def filter_datum(
         if new_field[0] in fields:
             message = re.sub(new_field[1], redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        m = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return m
