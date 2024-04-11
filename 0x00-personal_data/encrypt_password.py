1b1b086364936a5ab99df4443a9da26a88e69dbc
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-11 17:21:37 +0100

    changed annotation to byte instead of ByteString

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """A script that uses bcrypt to hash a password."""
 from bcrypt import gensalt, hashpw
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """
         A function that hashes a password.
         args:   password(str) the password to be hashed.
