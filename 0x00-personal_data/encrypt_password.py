72f8ea49122ece97e6ab8056b572ce3704eaa08e
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-11 17:37:51 +0100

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """A script that uses bcrypt to hash a password."""
-from bcrypt import gensalt, hashpw
+from bcrypt import gensalt, hashpw, checkpw
 
 
 def hash_password(password: str) -> bytes:
@@ -11,3 +11,13 @@ def hash_password(password: str) -> bytes:
     """
     hashed_passwd = hashpw(password.encode('utf-8'), gensalt())
     return hashed_passwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        A function that verifies that a password matches its hashed form.
+        args:   hashed_password(bytes) the hashed password.
+                password(str) the unhashed password.
+        return: True if it matches else false.
+    """
+    return checkpw(password.encode('utf-8'), hashed_password)
