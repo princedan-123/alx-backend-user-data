c0ea3da60c62d70b68835b96c27d4a48688cdc20
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-12 11:19:24 +0100

    changed import statement

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """A script that uses bcrypt to hash a password."""
-from bcrypt import gensalt, hashpw, checkpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
@@ -9,7 +9,7 @@ def hash_password(password: str) -> bytes:
         args:   password(str) the password to be hashed.
         return: A bytestring of the hashed password.
     """
-    hashed_passwd = hashpw(password.encode('utf-8'), gensalt())
+    hashed_passwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed_passwd
 
 
@@ -20,4 +20,4 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
                 password(str) the unhashed password.
         return: True if it matches else false.
     """
-    return checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
