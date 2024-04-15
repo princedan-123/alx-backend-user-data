2bc1035babe31dcdae54e47a28c34e0115ba042b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-15 19:43:50 +0100

    added a method to BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,18 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """A subclass of Auth."""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+            ) -> str:
+        """
+            A public method that extracts the encoded portion of Authorization
+            request header.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if 'Basic ' not in authorization_header:
+            return None
+        encoded_portion = authorization_header[6:]
+        return encoded_portion
