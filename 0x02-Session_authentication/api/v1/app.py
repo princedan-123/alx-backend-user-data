18f133a3ded2843688eba6b513b2bab4228354fb
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-17 15:00:42 +0100

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,11 +31,15 @@ def before_each_request():
     if auth is None:
         return
     exempted_list = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+        ]
 
     if not auth.require_auth(request.path, exempted_list):
         return
     if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.authorization_header(request) and auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
