c52973d2dfa44571f6ab0688f423a7fd1ace974b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-23 18:34:04 +0100

    task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -37,7 +37,7 @@ def login():
     password = request.form.get('password')
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
-        response = jsonify({"email": "<user email>", "message": "logged in"})
+        response = jsonify({"email": f"{email}", "message": "logged in"})
         response.set_cookie('session_id', session_id)
         return response
     else:
@@ -54,6 +54,19 @@ def logout():
         user = db.find_user_by(session_id=session_id)
         AUTH.destroy_session(user.id)
         return redirect('/')
+    except Exception:
+        abort(403)
+
+
+@app.route('/profile', strict_slashes=False)
+def profile():
+    """A route for acessing user profile."""
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+    try:
+        user = db.find_user_by(session_id=session_id)
+        return jsonify({"email": f"{email}"}), 200
     except Exception:
         abort(403)
 
