e2480e586218f189b05ffcf6bb189037f146c770
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-23 14:40:37 +0100

    implemented login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """A simple flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -26,6 +26,20 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError as error:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """A simple login in implementation"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": "<user email>", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        return abort(401)
 
 
 if __name__ == "__main__":
