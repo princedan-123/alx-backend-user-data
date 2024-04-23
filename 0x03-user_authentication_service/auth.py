18d007c87047c084fdf950d29f7d961de287d8cd
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-23 11:01:25 +0100

    fixed linter errors

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """A module for authentication."""
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +11,21 @@ def _hash_password(password: str) -> bytes:
     byte_string = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(byte_string, bcrypt.gensalt())
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """A method that hashes a users password, before adding."""
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound as error:
+            hashed_password = _hash_password(password)
+            registered_user = self._db.add_user(email, hashed_password)
+            return registered_user
