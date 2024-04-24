685e27189a7ce0c71fbeaf4a74954ecdf45704b1
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-24 10:34:49 +0100

    corrected method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,7 +86,7 @@ def get_reset_password_token():
         raise abort(403)
 
 
-@app.route('/reset_password', method=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """
     A route that updates a user's password in case of forgotten password.
